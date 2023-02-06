f4a533545f37aa4138d4e3f107c932a5aea26293
NEBULA1978 <102324836+NEBULA1978@users.noreply.github.com>
2023-02-06 21:36:28 +0100

    Add files via upload
    Segunda parte menu bash docker falta comprobar

--- menubase.sh
+++ menubase.sh
@@ -13,8 +13,17 @@ while :; do
     echo "6. Arranque de contenedores" 
     echo "7. Crear 2ºparte del menu parte de abajo de case" 
     echo "8. Ver 2º resultado opcion 7 en archivo resultado2.txt" 
-    echo "9. " 
-    echo "10. " 
+    echo "9. Listar los contenedores que se encuentren en funcionamientos" 
+    echo "10. Listar TODOS los contenedores(Parados y en funcionamiento)" 
+    echo "11. Ver información de un contenedor
+sudo docker inspect <id o nombre del contenedor>" 
+    echo "12. Parar un contenedor" 
+    echo "13. Poner en marcha otra vez un contenedor" 
+    echo "14. Eliminar un contenedor parado" 
+    echo "15.  Ver los logs de un contenedor" 
+
+
+
     echo "4. Salir"
     echo ""
     #Escoger menu
@@ -68,6 +77,53 @@ while :; do
         cat resultado2.txt
         read foo
         ;;
+        9)
+        echo "Listar los contenedores que se encuentren en funcionamientos"
+        docker ps
+        read foo
+        ;;
+    10)
+        echo "Listar TODOS los contenedores (Parados y en funcionamiento)"
+        docker ps -a
+        read foo
+        ;;
+    11)
+        echo "Ver información de un contenedor: sudo docker inspect <id o nombre del contenedor>"
+        echo "Introduce el ID o nombre del contenedor: "
+        read contenedor
+        docker inspect $contenedor
+        read foo
+        ;;
+    12)
+        echo "Parar un contenedor: sudo docker stop <id o nombre del contenedor>"
+        echo "Introduce el ID o nombre del contenedor: "
+        read contenedor
+        docker stop $contenedor
+        read foo
+        ;;
+    13)
+        echo "Poner en marcha otra vez un contenedor: sudo docker start <id o nombre del contenedor>"
+        echo "Introduce el ID o nombre del contenedor: "
+        read contenedor
+        docker start $contenedor
+        read foo
+        ;;
+    14)
+        echo "Eliminar un contenedor parado: sudo docker rm <id o nombre del contenedor>"
+        echo "Introduce el ID o nombre del contenedor: "
+        read contenedor
+        docker rm $contenedor
+        read foo
+        ;;
+    15)
+        echo "Ver los logs de un contenedor: sudo docker logs <id o nombre del contenedor>"
+        echo "Introduce el ID o nombre del contenedor: "
+        read contenedor
+        docker logs $contenedor
+        read foo
+        ;;
+
+
     4) exit 0 ;;
     #Alerta
     *)
